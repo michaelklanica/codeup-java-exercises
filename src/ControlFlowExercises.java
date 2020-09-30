8c8f6cdf6d20f7609c7570d258ceac5e8e4aba4d
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-30 15:22:10 -0500

    ControlFlowExercises - 4. Convert given number grades into letter grades

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,14 +4,20 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         while (true) {
             Scanner sc = new Scanner(System.in);
-            System.out.print("What number would you like to go up to? ");
-            int number = sc.nextInt();
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------- | -----");
-            for (int i = 1; i <= number; i++) {
-                int square = (int) Math.pow(i, 2);
-                int cube = (int) Math.pow(i, 3);
-                System.out.format("%-7d| %-8d| %d\n", i, square, cube);
+            System.out.print("Enter a grade from 0 to 100: ");
+            int grade = sc.nextInt();
+            if (grade < 0 || grade > 100) {
+                System.out.println("Invalid Grade");
+            } else if (grade <= 100 && grade >= 88) {
+                System.out.println("A");
+            } else if (grade <= 87 && grade >= 80) {
+                System.out.println("B");
+            } else if (grade <= 79 && grade >= 67) {
+                System.out.println("C");
+            } else if (grade <= 66 && grade >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
             }
             System.out.print("Would you like to continue? [y/n]");
             String response = sc.next();
