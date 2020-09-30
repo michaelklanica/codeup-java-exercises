ae55df5a5c69e74caf723b9e3b2cd2badf5cd0bf
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-29 23:03:57 -0500

    additional practice

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,37 +2,144 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        // Exercise 1
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f\n", pi);
 
-        // Exercise 2
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter something: ");
-        int userInput = scanner.nextInt();
-        System.out.printf("You entered %s.\n", userInput);
 
-        // Exercise 3
-        System.out.print("Enter three words: ");
-        String word1 = scanner.next();
-        String word2 = scanner.next();
-        String word3 = scanner.next();
-        System.out.printf("You entered %s %s %s\n", word1, word2, word3);
-        scanner.nextLine();     // This 'nextLine' captures the ENTER key; 'next' will only look for the next token--by
-                                // default the token is a space.
 
-        // Exercise 4
-        System.out.println("Enter a sentence: ");
-        String sentence = scanner.nextLine();
-        System.out.printf("%s is interesting.\n", sentence);
 
-        // Exercise 5
-        System.out.println("enter a length");
-        int length2 = Integer.parseInt(scanner.nextLine());
-        System.out.println("enter a width");
-        int width2 = Integer.parseInt(scanner.nextLine());
-        int perimeter2 = (length2 * 2) + (width2 * 2);
-        System.out.printf("The perimeter is %d", perimeter2);
+
+
+
+        // W3RESOURCE JAVA EXERCISES
+//        // 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+//        System.out.println("Hello\nMike");
+//
+//        // 2. Write a Java program to print the sum of two numbers.
+//        System.out.println(74 + 36);
+//
+//        // 3. Write a Java program to divide two numbers and print on the screen.
+//        System.out.println(50 / 3);
+//
+//        // 4. Write a Java program to print the result of the following operations.
+//        //Test Data:
+//        //a. -5 + 8 * 6
+//        System.out.println(-5 + 8 * 6);
+//        //b. (55+9) % 9
+//        System.out.println((55 + 9) % 9);
+//        //c. 20 + -3*5 / 8
+//        System.out.println(20 + -3 * 5 / 8);
+//        //d. 5 + 15 / 3 * 2 - 8 % 3
+//        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+//
+//        // 5. Write a Java program that takes two numbers as input and display the product of two numbers.
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int firstNum = sc.nextInt();
+//        System.out.print("Enter another number: ");
+//        int secondNum = sc.nextInt();
+//        System.out.println(firstNum * secondNum);
+//
+//        // 6. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+//        System.out.print("Enter a number: ");
+//        int number = sc.nextInt();
+//        System.out.print("Enter another number: ");
+//        int anotherNumber = sc.nextInt();
+//        System.out.println(number + " + " + anotherNumber + " = " + (number + anotherNumber));
+//        System.out.println(number + " - " + anotherNumber + " = " + (number - anotherNumber));
+//        System.out.println(number + " * " + anotherNumber + " = " + (number * anotherNumber));
+//        System.out.println(number + " / " + anotherNumber + " = " + (number / anotherNumber));
+//        System.out.println(number + " % " + anotherNumber + " = " + (number % anotherNumber));
+//
+//        // 7. Write a Java program that takes a number as input and prints its multiplication table up to 10.
+//        System.out.print("Enter a number: ");
+//        int num1 = sc.nextInt();
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(num1 + " x " + (i + 1) + " = " + (num1 * (i + 1)));
+//        }
+//
+//        // 8. Write a Java program to display the following pattern.
+//        //
+//        //    J    a   v     v  a
+//        //    J   a a   v   v  a a
+//        // J  J  aaaaa   V V  aaaaa
+//        //  JJ  a     a   V  a     a
+//        System.out.println("   J    a   v     v  a ");
+//        System.out.println("   J   a a   v   v  a a");
+//        System.out.println("J  J  aaaaa   V V  aaaaa");
+//        System.out.println(" JJ  a     a   V  a     a");
+//
+//        // 9. Write a Java program to compute the specified expressions and print the output.
+//        //((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+//        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+//
+//        // 10. Write a Java program to compute a specified formula.
+//        //4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+//        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+//
+//        // 11. Write a Java program to print the area and perimeter of a circle.
+//        double radius = 7.5;
+//
+//        double perimeter = 2 * Math.PI * radius;
+//        double area = Math.PI * radius * radius;
+//
+//        System.out.println("Perimeter is = " + perimeter);
+//        System.out.println("Area is = " + area);
+//
+//        // 12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
+//        System.out.println("Enter a number: ");
+//        int numA = sc.nextInt();
+//        System.out.println("Enter a number: ");
+//        int numB = sc.nextInt();
+//        System.out.println("Enter a number: ");
+//        int numC = sc.nextInt();
+//        System.out.println((numA + numB + numC)/3);
+//
+//        // 13. Write a Java program to print the area and perimeter of a rectangle.
+//        //Width = 5.5 Height = 8.5
+//        double heightA = 8.5;
+//        double widthA = 5.5;
+//
+//        double areaA = heightA * widthA;
+//        double periA = 2 * (heightA + widthA);
+//
+//        System.out.println(areaA);
+//        System.out.println(periA);
+
+
+// CODEUP CURRICULUM EXERCISES
+
+//        // Exercise 1
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f\n", pi);
+//
+//        // Exercise 2
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter something: ");
+//        int userInput = scanner.nextInt();
+//        System.out.printf("You entered %s.\n", userInput);
+//
+//        // Exercise 3
+//        System.out.print("Enter three words: ");
+//        String word1 = scanner.next();
+//        String word2 = scanner.next();
+//        String word3 = scanner.next();
+//        System.out.printf("You entered %s %s %s\n", word1, word2, word3);
+//        scanner.nextLine();     // This 'nextLine' captures the ENTER key; 'next' will only look for the next token--by
+//                                // default the token is a space.
+//
+//        // Exercise 4
+//        System.out.println("Enter a sentence: ");
+//        String sentence = scanner.nextLine();
+//        System.out.printf("%s is interesting.\n", sentence);
+//
+//        // Exercise 5
+//        System.out.println("enter a length");
+//        int length2 = Integer.parseInt(scanner.nextLine());
+//        System.out.println("enter a width");
+//        int width2 = Integer.parseInt(scanner.nextLine());
+//        int perimeter2 = (length2 * 2) + (width2 * 2);
+//        System.out.printf("The perimeter is %d", perimeter2);
+
     }
 }
