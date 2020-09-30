db8cd4af6a29b8f57f9e4c0b930a97015a7c3e12
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-30 13:46:32 -0500

    ControlFlowExercises - 2.FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,15 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        for (int i = 2; i <= 1000000; i = (int) Math.pow(i, 2)) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
