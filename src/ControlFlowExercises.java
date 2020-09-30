c013d909d89c6fb198de62fdf6c19ee0116eb8f2
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-30 13:35:13 -0500

    ControlFlowExercises - 1.Loop basics c. for pt2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,7 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        for (int i = 5; i <= 15; i++) {
-            System.out.print(i + " ");
+        for (int i = 2; i <= 1000000; i = (int) Math.pow(i, 2)) {
+            System.out.println(i);
         }
     }
 }
