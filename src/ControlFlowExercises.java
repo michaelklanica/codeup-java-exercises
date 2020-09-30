901884d687e1aa6c2da4cc17531c3320ea9a80a3
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-30 12:52:54 -0500

    ControlFlowExercises - 1.Loop basics b. do-while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,24 +1,9 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i + " ");
-            i++;
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        int i = 2;
+        do {
+            System.out.println(i);
+            i = (int) Math.pow(i, 2);
+        }while(i < 1000000);
     }
 }
