314536b590a1a5d96b26423addba243c69bca1ba
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-30 14:59:49 -0500

    ControlFlowExercises - 3.table of powers exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
-    public static void main (String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+    public static void main(String[] args) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("What number would you like to go up to? ");
+            int number = sc.nextInt();
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= number; i++) {
+                int square = (int) Math.pow(i, 2);
+                int cube = (int) Math.pow(i, 3);
+                System.out.format("%-7d| %-8d| %d\n", i, square, cube);
+            }
+            System.out.print("Would you like to continue? [y/n]");
+            String response = sc.next();
+            boolean confirm = response.equals("y");
+            if (!confirm) {
+                break;
             }
         }
     }
