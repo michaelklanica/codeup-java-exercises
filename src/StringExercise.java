bf77bb05fa74cd2fdaef2d2349ba72b624a1e7fe
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-30 19:51:38 -0500

    JAVA Strings exercise 1

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        // JAVA Strings - Exercise 1
+        String pinkFloyd  = "We don't need no education\nWe don't need no thought control";
+        System.out.println(pinkFloyd);
+
+        String quotesInsideQuotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotesInsideQuotes);
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
+
+    }
+}
