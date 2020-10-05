aff4c53c0086c2810a38877419390f02ffd02a72
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-05 11:10:11 -0500

    guessing game

---
+++ src/HighLow.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HighLow {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int range = 100 - 1 + 1;
+        int numberToGuess  = (int) ((Math.random() * range) + 1);
+        System.out.println("numberToGuess = " + numberToGuess);
+
+        while(true) {
+            System.out.println("guess a number between 1 and 100");
+            short userInput = Short.parseShort(scan.nextLine());
+            if (userInput > numberToGuess) {
+                System.out.println("LOWER");
+            } else if (userInput < numberToGuess) {
+                System.out.println("HIGHER");
+            } else {
+                System.out.println("GOOD GUESS");
+                break;
+            }
+
+
+
+
+        }
+
+
+    }
+}
