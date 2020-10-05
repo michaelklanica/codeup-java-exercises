20eb1b2fd51e3bb6c0fb91c8682a8500ce15c345
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-05 14:32:39 -0500

    OOP Short Assignment #1

--- src/Dish.java
+++ src/Dish.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Dish {
+public class Dish {
+
+    public int costInCents;
+    public String nameOfDish;
+    public boolean wouldRecommend;
+
+    public void printSummary() {
+        System.out.printf("Cost: " + costInCents + "\nName: " + nameOfDish + "\nWould Recommend: " + wouldRecommend);
+    }
 }
--- src/DishTest.java
+++ src/DishTest.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class DishTest {
+public class DishTest {
+    public static void main(String[] args){
+        Dish dish1 = new Dish();
+        dish1.costInCents = 650;
+        dish1.nameOfDish = "Enchiladas";
+        dish1.wouldRecommend = true;
+
+        dish1.printSummary();
+    }
 }
