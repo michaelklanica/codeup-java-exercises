23158e28664376f2a42a337b646350cf0db588a3
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-07 22:32:18 -0500

    Arrays exercise 2 complete

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,8 +1,23 @@
+
+import java.util.Random;
+
 public class ServerNameGenerator {
-    String[] nouns = {"man", "woman", "cat", "dog", "bird", "rockstar", "vehicle", "planet", "cloud", "rain"};
-    String[] adjectives = {"dark", "happy", "sad", "angry", "broken", "strong", "hairy", "proud", "huge", "tiny"};
+    public static String[] nouns = {"man", "woman", "cat", "dog", "bird", "rockstar", "vehicle", "planet", "cloud",
+            "rain"};
+    public static String[] adjectives = {"dark", "happy", "sad", "angry", "broken", "strong", "hairy", "proud", "huge",
+            "tiny"};
 
+    public static String getRandomServerName() {
+        int randomNoun = new Random().nextInt(10);
+        int randomAdjective = new Random().nextInt(10);
+        return adjectives[randomAdjective] + "-" + nouns[randomNoun];
+    }
 
+    public static void main(String[] args) {
+
+            System.out.println(getRandomServerName());
+
+    }
 
 
 
